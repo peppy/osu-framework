9b04f61543099d5390846fbd5570898aa151d80f
Dean Herbert <pe@ppy.sh>
2018-07-05 18:16:36 +0900

    Use switch to avoid cast
    Feels a bit more clear to read.

--- osu.Framework/Input/InputManager.cs
+++ osu.Framework/Input/InputManager.cs
@@ -115,7 +115,18 @@ namespace osu.Framework.Input
             RelativeSizeAxes = Axes.Both;
             foreach (var button in Enum.GetValues(typeof(MouseButton)).Cast<MouseButton>())
             {
-                var manager = button == MouseButton.Left ? new MouseLeftButtonEventManager(this, button) as MouseButtonEventManager : new MouseMinorButtonEventManager(this, button);
+                MouseButtonEventManager manager;
+
+                switch (button)
+                {
+                    case MouseButton.Left:
+                        manager = new MouseLeftButtonEventManager(this, button);
+                        break;
+                    default:
+                        manager = new MouseMinorButtonEventManager(this, button);
+                        break;
+                }
+
                 mouseButtonEventManagers.Add(button, manager);
             }
         }
