52543c024f80a7f409900e37dc8cdc30c0fa9429
Dean Herbert <pe@ppy.sh>
2025-08-25 18:07:54 +0900

    wip

--- osu.Framework.Tests/Audio/TrackBassWithDecoupledClockTest.cs
+++ osu.Framework.Tests/Audio/TrackBassWithDecoupledClockTest.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using ManagedBass;
 using NUnit.Framework;
 using osu.Framework.Audio.Track;
 using osu.Framework.Timing;
@@ -19,7 +20,7 @@ namespace osu.Framework.Tests.Audio
         [Test]
         public void TestSomething()
         {
-            var stopwatch = new StopwatchClock(true);
+            var stopwatch = new StopwatchClock();
 
             bass = new BassTestComponents();
             track = bass.GetTrack();
@@ -29,28 +30,37 @@ namespace osu.Framework.Tests.Audio
                 while (true)
                 {
                     bass.Update();
-                    await Task.Delay(1).ConfigureAwait(false);
+                    //await Task.Delay(1).ConfigureAwait(false);
                 }
             });
 
-            const double track_start_time = -1000;
+            const double track_start_time = -20000;
             const double elapsed_per_frame = 1;
             const double sync_lenience = 5;
 
             var decoupling = new DecouplingFramedClock(track);
-            decoupling.Seek(track_start_time);
-            decoupling.Start();
 
-            double startInRealTime = stopwatch.CurrentTime;
+            decoupling.Seek(track_start_time);
+            stopwatch.Seek(track_start_time);
+
+            stopwatch.Start();
+            decoupling.Start();
 
             while (decoupling.CurrentTime < 1000)
             {
                 decoupling.ProcessFrame();
 
-                double realtime = stopwatch.CurrentTime - startInRealTime + track_start_time;
+                //if (stopwatch.CurrentTime > -5)
+                {
+                    Console.WriteLine($"realtime: {stopwatch.CurrentTime:N2}");
+                    Console.WriteLine($"decouple: {decoupling.CurrentTime:N2}");
+                    Console.WriteLine($"drift   : {Math.Abs(decoupling.CurrentTime - stopwatch.CurrentTime):N2}");
+                    Console.WriteLine($"track   : {track.CurrentTime:N2}");
+                    Console.WriteLine();
+                }
 
-                if (Math.Abs(realtime - decoupling.CurrentTime) >= sync_lenience)
-                    Assert.Fail($"Decoupled clock desynchronised from real time. decoupled:{decoupling.CurrentTime:0.000}, realtime:{realtime:0.000}");
+                if (Math.Abs(stopwatch.CurrentTime - decoupling.CurrentTime) >= sync_lenience)
+                    Assert.Fail($"Decoupled clock desynchronised from real time. decoupled:{decoupling.CurrentTime:0.000}, realtime:{stopwatch.CurrentTime:0.000}");
 
                 Thread.Sleep((int)elapsed_per_frame);
             }
